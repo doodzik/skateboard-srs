ac20d0824e2a59302a549b63bd46f6beaaabe176
Frederik Dudzik <frederik@dudzik.co>
2018-04-29 18:05:22 -0400

    add selecting of tricks

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,22 +11,47 @@ import {
 
 import { CheckBox, Container, Header, Content, Button, Body, Left, Right, Title, Icon, List, ListItem, Text } from 'native-base';
 
-const datas = [
-  'Simon Mignolet',
-  'Nathaniel Clyne',
-  'Dejan Lovren',
-  'Mama Sakho',
-  'Alberto Moreno',
-  'Emre Can',
-  'Joe Allen',
-  'Phil Coutinho',
-];
+import Expo, { SQLite } from 'expo';
+const db = SQLite.openDatabase('db.db');
+
+class CheckboxedItem extends React.Component {
+  state = {
+    checked: false
+  }
+
+  toggleChecked() {
+    const checked = !this.state.checked
+    this.props.change(checked)
+    this.setState({checked})
+  }
+
+  render() {
+    return (<ListItem onPress={() => this.toggleChecked()}>
+      <CheckBox
+        onPress={() => this.toggleChecked()}
+        checked={this.state.checked}
+        />
+      <Body>
+        <Text>{this.props.name}</Text>
+      </Body>
+    </ListItem>)
+  }
+}
 
 export default class InboxScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
       title: 'SRS',
     }
+  }
+
+  state = {
+    items: null,
+    checked: new Set(),
+  };
+
+  componentDidMount() {
+    this.update();
   }
 
   check(secId, rowId, rowMap) {
@@ -44,7 +69,36 @@ export default class InboxScreen extends React.Component {
     this.setState({ listViewData: newData });
   }
 
+  update() {
+    db.transaction(tx => {
+      tx.executeSql(
+        `SELECT * FROM tricks;`, [],
+        (_, { rows: { _array } }) => {
+          this.setState({ items: _array })
+        });
+    });
+  }
+
+  toggleChecked(id) {
+    let _self = this
+    return checkboxState => {
+      let set = _self.state.checked
+      let checked = new Set(set)
+      if (checkboxState) { 
+        checked.add(id) 
+      } else { 
+        checked.delete(id)
+      }
+      _self.setState({checked})
+    }
+  }
+
   render() {
+    const { items } = this.state;
+    if (items === null || items.length === 0) {
+      return null
+    }
+
     return (
       <Container>
         <Header>
@@ -65,15 +119,9 @@ export default class InboxScreen extends React.Component {
           </Right>
         </Header>
         <Content>
-          <List dataArray={datas}
-            renderRow={(item) =>
-              <ListItem>
-                <CheckBox checked={false} />
-                <Body>
-                  <Text>{item}</Text>
-                </Body>
-              </ListItem>
-            }/>
+          <List dataArray={items}
+            renderRow={(item) => <CheckboxedItem name={item.name} change={this.toggleChecked(item.id)}/>}
+            />
         </Content>
       </Container>
     );
